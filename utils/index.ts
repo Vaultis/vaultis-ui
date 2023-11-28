28cffb350112ade708ef734e924f85e5c8ce31e5
[Nico242001] <nthiv077@uottawa.ca>
2023-11-28 12:15:46 -0500

    fixed some messages

--- utils/index.ts
+++ utils/index.ts
@@ -81,13 +81,13 @@ export function getAffordability(formData: any, mortgageInfo: Mortgage): Afforda
 
 function getMessage(salaryMinusExpenses:number, costs:number): String {
   if (salaryMinusExpenses - costs <= 0){
-    return "Cannot afford the property at all we recommend generating a minimum of " + (-1*(salaryMinusExpenses-costs)).toFixed(2) + " dollars per month in order to meet the minimum requirement to afford the property. Here are some possible solutions: finding a less expensive property, reduce expenses or increase income."
+    return "Cannot afford the property at all, we recommend generating a minimum of " + (-1*(salaryMinusExpenses-costs)).toFixed(2) + " dollars per month in order to meet the minimum requirement to afford the property. Here are some possible solutions: finding a less expensive property, reduce expenses or increase income."
   }
   if ((salaryMinusExpenses-costs)/costs <0.2) {
     const extraAmount = costs*0.2 - (salaryMinusExpenses - costs);
-    return "You can afford the property but we recommend generating an additional " + extraAmount.toFixed(2) + " dollars per month in order to comfortably afford the house. Here are some recommendations: reduce expenses or increase income."
+    return "You can afford the property, but we recommend generating an additional " + extraAmount.toFixed(2) + " dollars per month in order to comfortably afford the house. Here are some recommendations: reduce expenses or increase income."
   }
 
-  return "You can definitly afford the house it is well within your means."
+  return "You can definitly afford the property, it is well within your budget."
 
 }
